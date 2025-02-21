8b8e4b0f931f52e3f7e1899b987ee0511e8c87d5
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-21 15:24:23 +0100

    create array object bycicle

---
+++ Snack-1/main.js
@@ -0,0 +1,27 @@
+/* 
+
+Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+
+*/
+
+//  Create array object
+
+const bicicle = [
+{
+    'name' : 'Saetta',
+    'weight': '30'
+},
+{
+    'name' : 'mcQueen',
+    'weight' : '35'
+},
+{
+    'name' : 'crichetto',
+    'weight' : '20'
+},
+{
+    'name' : 'fenice',
+    'weight' : '23'
+}
+]

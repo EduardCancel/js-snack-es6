e5431f191f6515088236afad9bdbdeacc080ad30
EduardCancel <eduardcancel.boolean@gmail.com>
2025-02-21 15:42:46 +0100

    create array per le squadre

---
+++ Snack-2/main.js
@@ -0,0 +1,32 @@
+const suad = [
+    {
+        'nome': 'Milan',
+        'Punti fatti': '0',
+        'Falli subiti': '0'
+    },
+    {
+        'nome': 'Inter',
+        'Punti fatti': '0',
+        'Falli subiti': '0'
+    },
+    {
+        'nome': 'Juventus',
+        'Punti fatti': '0',
+        'Falli subiti': '0'
+    },
+    {
+        'nome': 'Fiorentina',
+        'Punti fatti': '0',
+        'Falli subiti': '0'
+    },
+    {
+        'nome': 'Napoli',
+        'Punti fatti': '0',
+        'Falli subiti': '0'
+    },
+    {
+        'nome': 'Atlanta',
+        'Punti fatti': '0',
+        'Falli subiti': '0'
+    },
+]
